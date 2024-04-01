263666cebddd071a1b53dd07f4d5424b4cb61af6
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-04-01 10:25:13 -0300

    Update Valuation_FIIs.py

--- pages/Valuation_FIIs.py
+++ pages/Valuation_FIIs.py
@@ -7,7 +7,6 @@ import matplotlib
 from matplotlib import style
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-import ssl
 
 st.page_link("Dash_Finance.py", label="Início", icon="🏠")
 
@@ -20,7 +19,6 @@ def Valuation_FIIs():
     plt.style.use('dark_background')
     plt.rcParams['figure.figsize'] = (18,8)
 
-    ssl._create_default_https_context = ssl._create_unverified_context
 
     def busca_titulos_tesouro_direto():
         url = 'https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv'
