a8017b5a70cf9b91c81c759ed7332327a369deed
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-04-10 10:34:37 -0300

    Update Valuation_FIIs.py

--- pages/Valuation_FIIs.py
+++ pages/Valuation_FIIs.py
@@ -107,9 +107,9 @@ with st.spinner('Carregando informações...'):
 
         valor_cota = pd.to_numeric(valor_cota)
         
-        st.write(f'O valor estimado da cota do :blue[{papel_maiusculo}] é :green[R${valor_cota:,.2f}]')
+        st.write(f'O valor estimado da cota do :blue[{papel_maiusculo}] é R${valor_cota:,.2f}')
         # crie deixar o resultado de var_dados aparecer em porcentagem
-        st.write(f'O valor de mercado do fundo :blue[{papel_maiusculo}] é :green[R${market_value:,.2f}], ou seja, uma diferença de :violet[{(var_dados):.2f}%] em relação ao valor estimado.' )
+        st.write(f'O valor de mercado do fundo :blue[{papel_maiusculo}] é R${market_value:,.2f}, ou seja, uma diferença de {(var_dados):.2f}% em relação ao valor estimado.' )
         
         
         fig1 = go.Figure(data=[go.Candlestick(x=dados.index,
