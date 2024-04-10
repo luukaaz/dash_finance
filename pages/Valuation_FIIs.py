82cb7df183cb3e947d3a032648727b4136d82a61
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-04-10 10:38:10 -0300

    Update Valuation_FIIs.py

--- pages/Valuation_FIIs.py
+++ pages/Valuation_FIIs.py
@@ -107,7 +107,7 @@ with st.spinner('Carregando informações...'):
 
         valor_cota = pd.to_numeric(valor_cota)
         
-        st.text(f'O valor estimado da cota do :blue[{papel_maiusculo}] é R${valor_cota:,.2f}')
+        st.write(f'O valor estimado da cota do :blue[{papel_maiusculo}] é R${valor_cota}')
         # crie deixar o resultado de var_dados aparecer em porcentagem
         st.write(f'O valor de mercado do fundo :blue[{papel_maiusculo}] é R${market_value:,.2f}, ou seja, uma diferença de {(var_dados):.2f}% em relação ao valor estimado.' )
         
