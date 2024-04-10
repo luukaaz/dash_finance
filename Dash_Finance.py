0875a5fcae14b35a0d7dd836d857fc3d2f7b4644
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-04-10 08:51:08 -0300

    Dash_Finance.py

--- Dash_Finance.py
+++ Dash_Finance.py
@@ -9,7 +9,7 @@ st.page_link("pages/Economia.py", label="Ver Indicadores Econômicos", icon="�
 
 st.page_link("pages/Indicadores.py", label="Ver Indicadores de Ações", icon="📊")
 
-st.page_link("pages/Mercado.py", label="Ir para Mercados Mundiais", icon="🌐💰")
+st.page_link("pages/Mercado.py", label="Ir para Mercados Mundiais", icon="🌐")
 
 st.page_link("pages/Valuation_FIIs.py", label="Avaliar um Fundo Imobiliário", icon="🏢")
 
