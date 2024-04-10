18eb2232bd783a174d1909df141678a4824438b8
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-04-10 08:44:03 -0300

    Dash_Finance.py

--- Dash_Finance.py
+++ Dash_Finance.py
@@ -1,14 +1,15 @@
-#Importação de Bibliotecas
 import streamlit as st
 
 
-st.title('Início')
+st.title('Início')  
 
 st.markdown('---')
 
-st.page_link("pages/Indicadores.py", label="Ver Indicadores", icon="📊")
+st.page_link("pages/Economia.py", label="Ver Indicadores Econômicos", icon="💰")
 
-st.page_link("pages/Mercado.py", label="Ir para Mercados", icon="💰")
+st.page_link("pages/Indicadores.py", label="Ver Indicadores de Ações", icon="📊")
+
+st.page_link("pages/Mercado.py", label="Ir para Mercados Mundiais", icon="🌐💰")
 
 st.page_link("pages/Valuation_FIIs.py", label="Avaliar um Fundo Imobiliário", icon="🏢")
 
