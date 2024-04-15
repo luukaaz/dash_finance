9306860b9702004bf8655a12d7866fdfad99a413
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-04-15 16:06:43 -0300

    Update Valuation_FIIs.py

--- pages/Valuation_FIIs.py
+++ pages/Valuation_FIIs.py
@@ -95,7 +95,7 @@ with st.spinner('Carregando informações...'):
         
         
         #Valor de Mercado
-        market_value = dados['Open'].iloc[-1]
+        market_value = dados.loc[:, 'Close'].iloc[-1]
         
         
         #Valor da Cota
