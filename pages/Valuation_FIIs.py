c1dd8da2bf948f208cd6a04d3018225d6ee1366b
Lucas <88948089+luukaaz@users.noreply.github.com>
2024-07-03 09:20:56 -0300

    Update Valuation_FIIs.py

--- pages/Valuation_FIIs.py
+++ pages/Valuation_FIIs.py
@@ -1,5 +1,7 @@
+# URL da API do Banco Central para obtenção de dados do IPCA
 ipca_url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json'
 
+# Importa as bibliotecas necessárias para o projeto
 import streamlit as st
 import pandas as pd
 import plotly
@@ -12,13 +14,18 @@ import plotly.graph_objects as go
 import numpy as np
 import numpy_financial as npf
 
+# Cria um link na página inicial do aplicativo
 st.page_link("Dash_Finance.py", label="Início", icon="🏠")
 
+# Define o título da página
 st.title(':green[Valuation FIIs]')
 
+# Adiciona uma linha horizontal como separador
 st.markdown('---')
 
+# Exibe um spinner (indicador de carregamento) enquanto as informações são carregadas
 with st.spinner('Carregando informações...'):
+    # Define uma função para buscar os títulos do Tesouro Direto
     def busca_titulos_tesouro_direto():
         url = 'https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv'
         df  = pd.read_csv(url, sep=';', decimal=',')
@@ -28,22 +35,27 @@ with st.spinner('Carregando informações...'):
         df = df.set_index(multi_indice).iloc[: , 3:]  
         return df
 
+    # Busca e ordena os títulos do Tesouro Direto
     titulos = busca_titulos_tesouro_direto()
     titulos.sort_index(inplace=True)
 
+    # Seleciona o título do Tesouro IPCA+ com vencimento em 2045
     ipca2045 = titulos.loc[('Tesouro IPCA+', '2045-05-15')]
 
-    #Spread mais recente do IPCA 2045
+    # Obtém o spread mais recente do IPCA 2045
     ultimo_ipca2045 = ipca2045['Taxa Compra Manha'][-1]
-    #Taxa IPCA
+    
+    # Obtém a taxa IPCA a partir da URL e processa os dados
     ipca_df = pd.read_json(ipca_url)
     ipca_index = ipca_df.set_index('data')
     ipca = ipca_index.iloc[-12:]
     ipca_ultimo = sum(ipca['valor'])
 
     
-    #Nome do Fundo
+    # Solicita o nome do fundo do usuário
     nome_fundo = st.text_input('Qual é o nome do fundo?')
+
+    # Solicita o tipo do fundo imobiliário do usuário
     tipo_fii = st.selectbox(
         "Selecione o tipo do fundo a ser avaliado",
         ("FII's Tijolo sem Crescimento", "FII's Tijolo com Crescimento", "FII's Papel"),
@@ -52,34 +64,38 @@ with st.spinner('Carregando informações...'):
     )
     
     st.markdown('---')
+
+    # Se o nome do fundo for fornecido
     if nome_fundo:
         papel_maiusculo = nome_fundo.upper()
         ativo = f'{papel_maiusculo}.SA'
 
+        # Solicita ao usuário se ele deseja inserir manualmente o provento do fundo
         div_estimativa = st.checkbox('Colocar manualmente o provento do fundo')
         st.caption(f'O modo automático utiliza o último provento pago pelo fundo, estimar manualmente pode fazer sentido se o último provento pago for um valor não recorrente. Cheque no gráfico de proventos abaixo se o valor é recorrente.')
 
-        
+        # Obtém os dados do fundo imobiliário utilizandoa biblioteca yfinance
         papel_fii = yf.Ticker(ativo)
         dados = papel_fii.history(period = '5y', interval = '1d')
         dados_filtrados = dados[dados['Dividends'] > 0]
         dados_filtrados = dados_filtrados.iloc[-12:]
         dados_filtrados = dados_filtrados['Dividends']
-        
+
+        # Cria e exibe um gráfico de barras da evolução dos proventos do fundo imobiliário
         fig = px.bar(x=dados_filtrados.index, y=dados_filtrados.values, template = 'plotly_dark', height = 400, width = 800, title = f'Evolução de Proventos do {papel_maiusculo}')
         st.plotly_chart(fig)
 
         if nome_fundo:
+            # Se o usuário optar por inserir manualmente o provento
             if div_estimativa:
                 dividendos = st.number_input("Digite o valor do provento a ser utilizado como base:")
                 provento_anual = dividendos*12
-
             else:
                 dividendos = dados_filtrados.tail(1)
                 provento_anual = dividendos*12
                 
             
-        #Prêmio de Risco
+        # Solicita ao usuário o prêmio de risco do fundo imobiliário
         # O Prêmio de Risco é considerado geralmente de 1.5% a 3.5% de acordo com o risco do fundo.
         premio_risco = st.number_input("Qual é o prêmio de risco do fundo imobiliário?", placeholder="prêmio de risco...")
         premio_risco_float = float(premio_risco)
@@ -87,15 +103,15 @@ with st.spinner('Carregando informações...'):
         
         st.markdown('---')
         
-        #Taxa de Desconto
+        # Calcula a taxa de desconto
         taxa_de_desconto = (premio_risco_float + ultimo_ipca2045)/100
         
         
-        #Valor de Mercado
+        # Obtém o valor de mercado do fundo
         market_value = dados.loc[:, 'Close'].iloc[-1]
         
         
-        #Valor da Cota
+        # Calcula o valor da cota com base no tipo do fundo imobiliário
         if tipo_fii ==  "FII's Papel":
             tx_desconto = (ultimo_ipca2045 + ipca_ultimo + premio_risco_float)
             valor_cota = (provento_anual/tx_desconto)*100
@@ -109,14 +125,14 @@ with st.spinner('Carregando informações...'):
             var_dados = ((market_value/valor_cota) -1)*100
         
         var_dados = float(((market_value/valor_cota) -1)*100)
-    
         valor_cota = float(valor_cota.iloc[-1])
         
+        # Exibe o valor estimado da cota do fundo
         st.write(f'O valor estimado da cota do :blue[{papel_maiusculo}] é :green[R${valor_cota:,.2f}]')
-        # crie deixar o resultado de var_dados aparecer em porcentagem
+        # Exibe a diferença percentual entre o valor de mercado e o valor estimado da cota
         st.write(f'O valor de mercado do fundo :blue[{papel_maiusculo}] é :green[R${market_value:,.2f}], ou seja, uma diferença de :violet[{(var_dados):.2f}%] em relação ao valor estimado.' )
         
-        
+        # Cria e exibe um gráfico de candlestick do valor da cota do fundo nos últimos 5 anos
         fig1 = go.Figure(data=[go.Candlestick(x=dados.index,
                             open=dados['Open'],
                             high=dados['High'],
